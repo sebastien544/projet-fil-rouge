61599805c16b2f7cb0a6a99b858a6db140e2755e
antoineulysse <antoineulysse@gmail.com>
2020-03-24 16:10:22 +0100

    harmonisation du controller

--- Back/Controller/checkout-page.php
+++ Back/Controller/checkout-page.php
@@ -1,3 +1,19 @@
+<?php
+
+session_start();
+
+	if (isset($_GET['action']) && $_GET['action']== 'logout'){
+		
+
+		session_destroy();
+
+		header('location:../../Controller/connexion-objet.php');
+		
+
+	}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,154 +36,156 @@
 
 <!--Navbar -->
 <nav class="mb-1 navbar navbar-expand-lg navbar-dark blue-grey">
-  <a class="navbar-brand" href="#">Save-Them.com</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
-    aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item ">
-        <a class="nav-link" href="#">Home
-          <span class="sr-only">(current)</span>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Donate</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Petition</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Shop</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">About us</a>
-      </li>
-      <li class="nav-item">
-      </li>
-    </ul>
- 
-          <button type="button" class="btn btn-blue-grey" data-toggle="modal" data-target="#modalCart"><i class="fas fa-shopping-cart"></i></button>
+		<a class="navbar-brand" href="http://localhost/projet-fil-rouge/Back/Antoine/php/gorilla.php">Save-Them.com</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+		  aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+		  <ul class="navbar-nav mr-auto">
+			<li class="nav-item ">
+			  <a class="nav-link" href="#">Home
+				<span class="sr-only">(current)</span>
+			  </a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="/projet-fil-rouge/Back/Controller/donation-page.php">Donate</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#">Petition</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="/projet-fil-rouge/Back/Antoine/php/shop_01.php">Shop</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="/projet-fil-rouge/Back/Antoine/php/contactus.php">About us</a>
+			</li>
+			<li class="nav-item">
+			</li>
+		  </ul>
+	   
+				
+				
+			
+		  <ul class="navbar-nav ml-auto nav-flex-icons">
+		  <a class="btn-floating btn-lg"  data-toggle="modal" data-target="#modalCart"><span style="color: white"><i class="fas fa-shopping-cart"></i></span></a>
+	  
+				  <!-- Modal: modalCart -->
+				  <div class="modal fade" id="modalCart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+					aria-hidden="true">
+					<div class="modal-dialog" role="document">
+					  <div class="modal-content">
+						<!--Header-->
+						<div class="modal-header">
+						  <h4 class="modal-title" id="myModalLabel">Your cart</h4>
+						  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">×</span>
+						  </button>
+						</div>
+						<!--Body-->
+						<div class="modal-body">
+	  
+						  <table class="table table-hover">
+							<thead>
+							  <tr>
+								<th>#</th>
+								<th>Product name</th>
+								<th>Price</th>
+								<th>Remove</th>
+							  </tr>
+							</thead>
+							<tbody>
+							  <tr>
+								<th scope="row">1</th>
+								<td>Product 1</td>
+								<td>100$</td>
+								<td><a><i class="fas fa-times"></i></a></td>
+							  </tr>
+							  <tr>
+								<th scope="row">2</th>
+								<td>Product 2</td>
+								<td>100$</td>
+								<td><a><i class="fas fa-times"></i></a></td>
+							  </tr>
+							  <tr>
+								<th scope="row">3</th>
+								<td>Product 3</td>
+								<td>100$</td>
+								<td><a><i class="fas fa-times"></i></a></td>
+							  </tr>
+							  <tr>
+								<th scope="row">4</th>
+								<td>Product 4</td>
+								<td>100$</td>
+								<td><a><i class="fas fa-times"></i></a></td>
+							  </tr>
+							  <tr class="total">
+								<th scope="row">5</th>
+								<td>Total</td>
+								<td>400$</td>
+								<td></td>
+							  </tr>
+							</tbody>
+						  </table>
+	  
+						</div>
+						<!--Footer-->
+						<div class="modal-footer">
+						  <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Close</button>
+						  <button class="btn btn-primary">Checkout</button>
+						</div>
+					  </div>
+					</div>
+				  </div>
 
-            <!-- Modal: modalCart -->
-            <div class="modal fade" id="modalCart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-              aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <!--Header-->
-                  <div class="modal-header">
-                    <h4 class="modal-title" id="myModalLabel">Your cart</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                      <span aria-hidden="true">×</span>
-                    </button>
-                  </div>
-                  <!--Body-->
-                  <div class="modal-body">
-
-                    <table class="table table-hover">
-                      <thead>
-                        <tr>
-                          <th>#</th>
-                          <th>Product name</th>
-                          <th>Price</th>
-                          <th>Remove</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <th scope="row">1</th>
-                          <td>Product 1</td>
-                          <td>100$</td>
-                          <td><a><i class="fas fa-times"></i></a></td>
-                        </tr>
-                        <tr>
-                          <th scope="row">2</th>
-                          <td>Product 2</td>
-                          <td>100$</td>
-                          <td><a><i class="fas fa-times"></i></a></td>
-                        </tr>
-                        <tr>
-                          <th scope="row">3</th>
-                          <td>Product 3</td>
-                          <td>100$</td>
-                          <td><a><i class="fas fa-times"></i></a></td>
-                        </tr>
-                        <tr>
-                          <th scope="row">4</th>
-                          <td>Product 4</td>
-                          <td>100$</td>
-                          <td><a><i class="fas fa-times"></i></a></td>
-                        </tr>
-                        <tr class="total">
-                          <th scope="row">5</th>
-                          <td>Total</td>
-                          <td>400$</td>
-                          <td></td>
-                        </tr>
-                      </tbody>
-                    </table>
-
-                  </div>
-                  <!--Footer-->
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Close</button>
-                    <button class="btn btn-primary">Checkout</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- Modal: modalCart -->
-      
-    <ul class="navbar-nav ml-auto nav-flex-icons">
-   
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-user"></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg-right dropdown-menu-sm-left dropdown-default"
-          aria-labelledby="navbarDropdownMenuLink-333">
-          <form class="text-center border border-light p-5" action="#!">
-
-            <p class="h4 mb-4">Sign in</p>
-        
-            <!-- Email -->
-            <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
-        
-            <!-- Password -->
-            <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
-        
-            <div class="d-flex justify-content-around">
-                <div>
-                    <!-- Remember me -->
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
-                        <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
-                    </div>
-                </div>
-                <div>
-                    <!-- Forgot password -->
-                    <a href="">Forgot password?</a>
-                </div>
-            </div>
-        
-            <!-- Sign in button -->
-            <button class="btn btn-info btn-block my-4 btn btn-blue-grey" type="submit">Sign in</button>
-        
-            <!-- Register -->
-            <p>Not a member?
-               <br> <a href="">Register</a>
-            </p>
-        
-        </form>
-
-        </div>
-      </li>
-
-    </ul>
-  </div>
-</nav>
+		  <a class="btn-floating btn-lg " href="../../Controller/connexion-objet.php?action=logout"><span style="color: white"><i class="fas fa-sign-out-alt"></i></span></a>
+			<li class="nav-item dropdown">
+			  <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+				aria-haspopup="true" aria-expanded="false">
+				<i class="fas fa-user"></i>
+			  </a>
+			  <div class="dropdown-menu dropdown-menu-lg-right dropdown-menu-sm-left dropdown-default"
+				aria-labelledby="navbarDropdownMenuLink-333">
+				<form class="text-center border border-light p-5" method="POST" action="/projet-fil-rouge/Back/Controller/authentification.php">
+	  
+				  <p class="h4 mb-4">Sign in</p>
+			  
+				  <!-- Email -->
+				  <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail" name="mail">
+			  
+				  <!-- Password -->
+				  <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password" name="password">
+			  
+				  <div class="d-flex justify-content-around">
+					  <div>
+						  <!-- Remember me -->
+						  <div class="custom-control custom-checkbox">
+							  <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+							  <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+						  </div>
+					  </div>
+					  <div>
+						  <!-- Forgot password -->
+						  <a href="">Forgot password?</a>
+					  </div>
+				  </div>
+			  
+				  <!-- Sign in button -->
+				  <button class="btn btn-info btn-block my-4 btn btn-blue-grey" type="submit" name="validation">Sign in</button>
+			  
+				  <!-- Register -->
+				  <p>Not a member?
+					 <br> <a href="/../../projet-fil-rouge/Back/Controller/signup-page.php">Register</a>
+				  </p>
+			  
+			  </form>
+	  
+			  </div>
+			</li>
+	  
+		  </ul>
+		</div>
+	  </nav>
 <!--/.Navbar -->
 
   <!--Main layout-->
--- Back/Controller/donation-page.php
+++ Back/Controller/donation-page.php
@@ -1,5 +1,5 @@
 <?php
-include("../Service/UtilisateurService.php");
+include_once ("../Service/UtilisateurService.php");
 
 session_start();
 
@@ -15,7 +15,7 @@ session_start();
   }
   
   if($_POST){
-    if($_SESSION){
+    if(isset($_SESSION['mail'])){
       $userServ = new UtilisateurService;
       $userServ->ajout_don($_POST, $_SESSION['mail']);
     }
@@ -63,68 +63,174 @@ session_start();
     </style>
   </head>
   <body>
-    <nav class="mb-1 navbar navbar-expand-lg navbar-dark blue-grey">
-      <a class="navbar-brand" href="#">Save-Them.com</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
-        aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="/projet-fil-rouge/Back/Home/Home.php">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="donation-page.php">Donate</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Petition</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../Antoine/php/shop_03.php">Shop</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="../Antoine/php/contactus.php">About us</a>
-          </li>
-          <li class="nav-item">
-          </li>
-        </ul>
-        <ul class="navbar-nav ml-auto nav-flex-icons">
-      <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-user"></i>
-            </a>
-            <div id="myModal"  class="dropdown-menu dropdown-menu-lg-right dropdown-menu-sm-left dropdown-default"
-              aria-labelledby="navbarDropdownMenuLink-333">
-              <form class="text-center border border-light p-5" method="POST" action="/projet-fil-rouge/Back/Controller/authentification.php">
-                <p class="h4 mb-4">Sign in</p>
-            <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail" name="mail">
-            <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password" name="password">
-            <div class="d-flex justify-content-around">
-                    <div>
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
-                            <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
-                        </div>
-                    </div>
-                    <div>
-                      <a href="">Forgot password?</a>
-                    </div>
-                </div>
-            <button class="btn btn-info btn-block my-4 btn btn-blue-grey" type="submit" name="validation">Sign in</button>
-            <p>Not a member?
-                  <br> <a href="/../../projet-fil-rouge/Back/Controller/signup-page.php">Register</a>
-                </p>
-            </form>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <a class="btn-floating btn-lg " href="/projet-fil-rouge/Back/Home/Home.php"><span style="color: white"><i class="fas fa-sign-out-alt"></i></span></a>
-    </nav>
+  <nav class="mb-1 navbar navbar-expand-lg navbar-dark blue-grey">
+		<a class="navbar-brand" href="/projet-fil-rouge/Back/Antoine/php/gorilla.php">Save-Them.com</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+		  aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+		  <ul class="navbar-nav mr-auto">
+			<li class="nav-item ">
+			  <a class="nav-link" href="#">Home
+				<span class="sr-only">(current)</span>
+			  </a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="/projet-fil-rouge/Back/Controller/donation-page.php">Donate</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#">Petition</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="/projet-fil-rouge/Back/Antoine/php/shop_01.php">Shop</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="/projet-fil-rouge/Back/Antoine/php/contactus.php">About us</a>
+			</li>
+			<li class="nav-item">
+			</li>
+		  </ul>
+	   
+				
+				
+			
+		  <ul class="navbar-nav ml-auto nav-flex-icons">
+		  <a class="btn-floating btn-lg"  data-toggle="modal" data-target="#modalCart"><span style="color: white"><i class="fas fa-shopping-cart"></i></span></a>
+	  
+				  <!-- Modal: modalCart -->
+				  <div class="modal fade" id="modalCart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+					aria-hidden="true">
+					<div class="modal-dialog" role="document">
+					  <div class="modal-content">
+						<!--Header-->
+						<div class="modal-header">
+						  <h4 class="modal-title" id="myModalLabel">Your cart</h4>
+						  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">×</span>
+						  </button>
+						</div>
+						<!--Body-->
+						<div class="modal-body">
+	  
+						  <table class="table table-hover">
+							<thead>
+							  <tr>
+								<th>#</th>
+								<th>Product name</th>
+								<th>Price</th>
+								<th>Remove</th>
+							  </tr>
+							</thead>
+							<tbody>
+							  <tr>
+								<th scope="row">1</th>
+								<td>Product 1</td>
+								<td>100$</td>
+								<td><a><i class="fas fa-times"></i></a></td>
+							  </tr>
+							  <tr>
+								<th scope="row">2</th>
+								<td>Product 2</td>
+								<td>100$</td>
+								<td><a><i class="fas fa-times"></i></a></td>
+							  </tr>
+							  <tr>
+								<th scope="row">3</th>
+								<td>Product 3</td>
+								<td>100$</td>
+								<td><a><i class="fas fa-times"></i></a></td>
+							  </tr>
+							  <tr>
+								<th scope="row">4</th>
+								<td>Product 4</td>
+								<td>100$</td>
+								<td><a><i class="fas fa-times"></i></a></td>
+							  </tr>
+							  <tr class="total">
+								<th scope="row">5</th>
+								<td>Total</td>
+								<td>400$</td>
+								<td></td>
+							  </tr>
+							</tbody>
+						  </table>
+	  
+						</div>
+						<!--Footer-->
+						<div class="modal-footer">
+						  <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Close</button>
+						  <button class="btn btn-primary">Checkout</button>
+						</div>
+					  </div>
+					</div>
+				  </div>
+
+		  <a class="btn-floating btn-lg " href="../../Controller/connexion-objet.php?action=logout"><span style="color: white"><i class="fas fa-sign-out-alt"></i></span></a>
+			<!--Dropdown primary-->
+			<li class="nav-item dropdown" id="menuDiv">
+				<!--Trigger-->
+				<a class="nav-link dropdown-toggle"  id="dropdownMenu1" data-toggle="dropdown"
+				aria-haspopup="true" aria-expanded="false"><i class="fas fa-user"></i></a>
+				<div class="dropdown-menu dropdown-menu-lg-right dropdown-menu-sm-left dropdown-default">
+
+							<!--Menu-->
+							
+							<a class="dropdown-item" href="/projet-fil-rouge/Back/Controller/personal-space.php">Donation History</a>
+							<a class="dropdown-item" href="/projet-fil-rouge/Back/Controller/personal-space.php">Petition History</a>
+							<a class="dropdown-item" href="/projet-fil-rouge/Back/Controller/personal-space.php">Personal Informations</a>
+						
+						
+					</div>	
+			</li>
+				<!--/Dropdown primary-->
+		  <li class="nav-item dropdown" id="loginDiv" >
+			  <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+				aria-haspopup="true" aria-expanded="false">
+				<i class="fas fa-user"></i>
+			  </a>
+			  <div id="myModal" class="dropdown-menu dropdown-menu-lg-right dropdown-menu-sm-left dropdown-default"
+				aria-labelledby="navbarDropdownMenuLink-333">
+				<form class="text-center border border-light p-5" method=POST action="/projet-fil-rouge/Back/Controller/donation-page.php">
+	  
+				  <p class="h4 mb-4">Sign in</p>
+			  
+				  <!-- Email -->
+				  <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail" name="mail">
+			  
+				  <!-- Password -->
+				  <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password"  name="password">
+			  
+				  <div class="d-flex justify-content-around">
+					  <div>
+						  <!-- Remember me -->
+						  <div class="custom-control custom-checkbox">
+							  <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+							  <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+						  </div>
+					  </div>
+					  <div>
+						  <!-- Forgot password -->
+						  <a href="">Forgot password?</a>
+					  </div>
+				  </div>
+			  
+				  <!-- Sign in button -->
+				  <button class="btn btn-info btn-block my-4 btn btn-blue-grey" type="submit" name="validation" id="jquery-toggle" id="clicked">Sign in</button>
+			  
+				  <!-- Register -->
+				  <p>Not a member?
+					 <br> <a href="/../../projet-fil-rouge/Back/Controller/signup-page.php">Register</a>
+				  </p>
+			  
+			  </form>
+	  
+			  </div>
+			</li>
+	  
+		  </ul>
+		</div>
+	  </nav>
     
     
     
@@ -212,6 +318,10 @@ session_start();
     <script type="text/javascript" src="js/popper.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/mdb.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.js"
+	integrity="sha256-BTlTdQO9/fascB1drekrDVkaKd9PkwBymMlHOiG+qLI="
+	crossorigin="anonymous"></script>
     <script type="text/javascript">
     new WOW().init();
     $("button").click(function(e){
