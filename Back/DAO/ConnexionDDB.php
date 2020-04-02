f049ede9eb85e45c1bb2984dd82fb42859a1f8ea
antoineulysse <antoineulysse@gmail.com>
2020-04-02 15:44:06 +0200

    corrections bug sur petition et shop

--- Back/DAO/ConnexionDDB.php
+++ Back/DAO/ConnexionDDB.php
@@ -4,7 +4,7 @@ class ConnexionDDB {
 
 
     function connectDatabase(){
-        $db = new mysqli( 'localhost', 'root', '', 'save_them');
+        $db = new mysqli( '127.0.0.1', 'root', '200386', 'save_them');
         return $db;
     }
 }
